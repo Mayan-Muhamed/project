c7800959d1a6c80ffeafae64cf2e7699aaebba58
Bakir <bakirbazarkul16@gmail.com>
2023-06-25 22:39:52 +0300

    Создал контейнер разделил на 5 блоков

--- src/Components/UI/App.jsx
+++ src/Components/UI/App.jsx
@@ -1,9 +1,82 @@
 import React from "react"
+import AppStyle from './AppStyle.module.css'
 function App() {
   return (
-    <>
-     Start
-    </>
+    <div className={AppStyle.contain}>
+     <div 
+       style={{  height: `200px`, margin: `50px auto`, border: `2px solid black`,display: "flex",
+        justifyContent: "center",
+        alignItems: "center",}}> 
+
+    Этот блок  для  Бакира.! <br/> 
+    Надо удалить всё что находиться  в  этом  блоке <br/>
+     и загрузить свой стилизованный  компонент <br/>
+    .Стили тоже нада удалить.<br/>
+    <br/>
+    <br/>
+    Желаю удачи в проекте  ))
+
+
+    </div>
+
+       <div style={{  height: `200px`, margin: `50px auto`,border: `2px solid black`,display: "flex",
+        justifyContent: "center",
+        alignItems: "center", }}> 
+
+        Этот блок  для  Мухамеда.! <br/> 
+        Надо удалить всё что находиться  в  этом  блоке <br/>
+        и загрузить свой стилизованный  компонент <br/>
+        .Стили тоже нада удалить. 
+        <br/>
+        <br/>
+        Желаю удачи в проекте  ))
+
+
+    </div>
+
+         <div style={{  height: `200px`, margin: `50px auto`,border: `2px solid black`,display: "flex",
+        justifyContent: "center",
+        alignItems: "center", }}> 
+
+        Этот блок  для  Далиля.! <br/> 
+        Надо удалить всё что находиться  в  этом  блоке <br/>
+        и загрузить свой стилизованный  компонент <br/>
+        .Стили тоже нада удалить. 
+        <br/>
+        <br/>
+        Желаю удачи в проекте  ))
+
+    </div>
+
+       <div style={{  height: `200px`, margin: `50px auto`,border: `2px solid black`,display: "flex",
+        justifyContent: "center",
+        alignItems: "center", }}> 
+
+        Этот блок  для  Игрья.! <br/> 
+        Надо удалить всё что находиться  в  этом  блоке <br/>
+        и загрузить свой стилизованный  компонент <br/>
+        .Стили тоже нада удалить.
+        <br/>
+        <br/>
+        Желаю удачи в проекте  )) 
+
+    </div>
+
+     <div style={{  height: `200px`, margin: `50px auto`,border: `2px solid black`,display: "flex",
+        justifyContent: "center",
+        alignItems: "center", }}> 
+
+        Этот блок  для  Асема.! <br/> 
+        Надо удалить всё что находиться  в  этом  блоке <br/>
+        и загрузить свой стилизованный  компонент <br/>
+        .Стили тоже нада удалить. 
+        <br/>
+        <br/>
+        Желаю удачи в проекте  ))
+
+    </div>
+
+    </div>
   )
 }
 
