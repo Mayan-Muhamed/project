420c4a5cc4bf2689c9ba05ac3918f79c5a6c3a36
Bakir <bakirbazarkul16@gmail.com>
2023-06-26 21:37:30 +0300

    Добавиль файл .prettierrc

--- src/Components/UI/Title.jsx
+++ src/Components/UI/Title.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import TitleStyle from './TitleStyle.module.css'
-const Title = ({title ,item}) =>{
-     return(<>
-     <div className={TitleStyle.title}>{title} <span className={TitleStyle.p}>{item}</span></div>
-    </>)
+const Title = ({ title, item }) => {
+
+    return (<div className={TitleStyle.title}>{title} <span className={TitleStyle.p}>{item}</span></div>)
 }
 export default Title
