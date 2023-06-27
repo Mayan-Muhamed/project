726ff432967f321218c19d52dbd07ffc198003b0
Bakir <bakirbazarkul16@gmail.com>
2023-06-27 18:30:10 +0300

    import Button

--- src/Components/UI/Button.jsx
+++ src/Components/UI/Button.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-const Button = ({textButton ,w ,h ,bg}) => {
- let   ButtonStyle = {
-      width:w +'px',
-      height: h + 'px',
-      background:bg,
-      fontFamily:  `'Open Sans', sans-serif`,
-      color:'#fff'
-    }
-    return(<>
+const Button = ({ textButton, w, h, bg }) => {
+  let ButtonStyle = {
+    width: w + 'px',
+    height: h + 'px',
+    background: bg,
+    fontFamily: `'Open Sans', sans-serif`,
+    color: '#fff'
+  }
+  return (<>
     <button style={ButtonStyle}>{textButton}</button>
-    </>)
+  </>)
 }
 export default Button
